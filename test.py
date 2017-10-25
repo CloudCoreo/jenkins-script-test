2ea411cc36b6f4e0a5d299721a472f706108b3c9
Andrew Armaneous <andrew@cloudcoreo.com>
2017-10-25 11:18:30 -0700

    Changed to creating cloudformation stack

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 import boto3
 
-s3 = boto3.client('s3')
+cf = boto3.client('cloudformation')
 
-print('Creating bucket')
-response = s3.create_bucket(Bucket='aa-coreo-test')
-print('Successfully created bucket')
+print('Creating cloudformation stack')
+response = cf.create_stack(
+    StackName='aa-stack-test',
+    TemplateURL='https://s3.amazonaws.com/aa-coreo-test/demo_agent.json',
+    Parameters=[
+        {
+            'ParameterKey': 'CCAgentTokenKeyID',
+            'ParameterValue': '599b8bfa28e49900d98abd8d'
+        },
+        {
+            'ParameterKey': 'CCAgentTokenSecretKey',
+            'ParameterValue': 'f25cfc2fe338f86b7ea6'
+        }
+    ]
+    
+)
+print('Successfully created stack:')
+print(response)
